111fd23312b906c9229e23e4b3b329a9c98361f1
unknown <ryli@nx.com>
2025-01-14 17:43:57 +0800

    commit

--- 03_链表/08_双指针法_链表相交.ts
+++ 03_链表/08_双指针法_链表相交.ts
@@ -9,6 +9,10 @@ const getIntersectionNode = function (
   let currA: ListNode | null = headA
   let currB: ListNode | null = headB
 
+  // 相交部分长度为k listA长度为n listB长度为m
+  // 不相交的部分为n - k 以及 m - k
+  // 走到相交结点 (currA -> n + m - k) (currB -> m + n - k)
+  // 不相交时 遍历完两个链表 同时走到null 不会死循环
   while (currA !== currB) {
     currA = currA === null ? headB : currA.next
     currB = currB === null ? headA : currB.next
